df03fff3f814798f9a9853beb9764330730c8e3d
Le Duc Hung <d.le@dsg.tuwien.ac.at>
2015-02-25 09:40:40 +0100

    graphic

--- salsa-core-pom/salsa-engine/src/main/java/at/ac/tuwien/dsg/cloud/salsa/engine/services/jsondata/ServiceJsonDataForceDirect.java
+++ salsa-core-pom/salsa-engine/src/main/java/at/ac/tuwien/dsg/cloud/salsa/engine/services/jsondata/ServiceJsonDataForceDirect.java
@@ -1,38 +1,48 @@
 package at.ac.tuwien.dsg.cloud.salsa.engine.services.jsondata;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ServiceJsonDataForceDirect {
 	
-	List<Node> nodes = new ArrayList<ServiceJsonDataForceDirect.Node>();
-	List<Link> links = new ArrayList<ServiceJsonDataForceDirect.Link>();
+	List<Node> nodes = new ArrayList<>();
+	List<Link> links = new ArrayList<>();
+        Map<String, Integer> nodeIndexMap = new HashMap<>();
+        int nodeIndexCount=0;
 	
 	public class Node{		
 		String name;
 		String group;
-		public Node(String name, String group){
+                String state;
+                String type;
+		public Node(String name, String group, String type, String state){
 			this.name = name;
 			this.group = group;
+                        this.state = state;
+                        this.type = type;
 		}
 	}
 	
 	public class Link{
-		String source;
-		String target;
+		int source;
+		int target;
 		String type;
-		public Link(String source, String target, String type){
+		public Link(int source, int target, String type){
 			this.source = source;
 			this.target = target;
-			this.type = type;			
+			this.type = type;	
 		}
 	}
 	
-	public void addNode(String name, String group){
-		this.nodes.add(new Node(name,group));
+	public void addNode(String name, String group, String type, String state){                
+		this.nodes.add(new Node(name,group,type,state));
+                nodeIndexMap.put(name, nodeIndexCount);
+                nodeIndexCount+=1;
 	}
 	
 	public void addLink(String source, String target, String type){
-		this.links.add(new Link(source, target, type));
+		this.links.add(new Link(nodeIndexMap.get(source), nodeIndexMap.get(target), type));
 	}
 }
